a9dd4b446d907bce924046b986fcaa45a5427898
Ariel Aguilar <arielaguilar1994@gmail.com>
2026-05-12 16:37:49 +0200

    feat: implement user verification endpoint with AuthGuard and CurrentUser interface

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,11 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post, Req, UseGuards } from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
-import { firstValueFrom } from 'rxjs';
+import { catchError, firstValueFrom } from 'rxjs';
 import { NATS_SERVICE } from 'src/config';
 import { LoginUserDto, RegisterUserDto } from './dto';
+import { AuthGuard } from './guard/auth.guard';
+import { Token, User } from './decorators';
+import type { CurrentUser } from './intefaces/current-user.interface';
 
 @Controller('auth')
 export class AuthController {
@@ -36,16 +39,14 @@ export class AuthController {
     }
   }
 
-  @Post('verify')
-  async verify(@Body() userDto: LoginUserDto) {
-    try {
-      const result = await firstValueFrom(
-        this.client.send('auth.verify.user', userDto)
-      );
+  @UseGuards( AuthGuard )
+  @Get('verify')
+  async verify(@User() user: CurrentUser, @Token() token: string ) {
 
-      return result;
-    } catch (error) {
-      throw new RpcException(error as string | object);
-    }
+    return this.client.send('auth.verify.user', { user, token }).pipe(
+      catchError((err) => {
+        throw new RpcException(err as string | object);
+      })
+    );
   }
 }
